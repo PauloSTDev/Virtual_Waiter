4f6f7d07aac9dc83f76bd986793d66db26285437
paulo.santosDell <afonso.mea@gmail.com>
2022-06-17 11:42:35 -0300

    Adiciona conteúdo sobre a Arquitetura

--- src/screens/screen_users/Arquitetura.js
+++ src/screens/screen_users/Arquitetura.js
@@ -7,22 +7,57 @@ export default function Arquitetura() {
       <SafeAreaView>
         <ScrollView>
           <Text style={styles.titulo}>
-            Arquitetura é baseada em dividir o máximo possivel os serviços e pastas do projeto por sua devida responsabilidade:
+            A arquitetura é baseada em dividir o máximo possivel os serviços e pastas do projeto por
+            sua devida responsabilidade, afim de facilitar a manutenção, comprender melhor o código
+            e o entender a importância de cada arquivo.
           </Text>
           <Text style={styles.titulo}>
-            Dentro da pasta src teremos back-end, components, screens e services.
+            Dentro da pasta src temos 4 pastas, sendo: back-end, components, screens e services.
           </Text>
           <Text style={styles.titulo}>
-            Em back-end existe o arquivo de configurações da conexão com o Firebase.
+            Em back-end:
+          </Text>
+          <Text style={styles.texto}>
+            Contem o arquivo das configurações de conexão com o Firebase.
           </Text>
           <Text style={styles.titulo}>
-            Components: Contêm os components que serão usados no App afim de facilitar a manutenção.
+            Components:
+          </Text>
+          <Text style={styles.texto}>
+            Contêm os components que serão usados no App.
           </Text>
           <Text style={styles.titulo}>
-            Screens: Temos uma pasta para screens de acesso Admin e outra para users normais contendo seus devidos arquivos.
+            Screens:
+          </Text>
+          <Text style={styles.texto}>
+            Temos uma pasta para screens de acesso Admin e outra para users normais contendo seus devidos arquivos.
           </Text>
           <Text style={styles.titulo}>
-            Services: Teremos 4 pastas, firebase_authentication_service que é responsavel pela autenticação de logoff, firebase_firestore_database_services, firebase_storage_services e google_geocoding_api_services contendo seus devidos arquivos e responsabilidades.
+            Services é composto por 4 pastas:
+          </Text>
+          <Text style={styles.subtitulo}>
+            firebase_authentication_service:
+          </Text>
+          <Text style={styles.texto}>
+            Responsável pela autenticação de Login, Logoff e CreateUser.
+          </Text>
+          <Text style={styles.subtitulo}>
+            firebase_firestore_database_services:
+          </Text>
+          <Text style={styles.texto}>
+            Responsável pelo Get e Post do Firebase.
+          </Text>
+          <Text style={styles.subtitulo}>
+            firebase_storage_services:
+          </Text>
+          <Text style={styles.texto}>
+            Responsável pelo Get de Imagens do Firebase.
+          </Text>
+          <Text style={styles.subtitulo}>
+            google_geocoding_api_services:
+          </Text>
+          <Text style={styles.texto}>
+            Responsável pela pesquisa de latitude e longitude e validação de endereço.
           </Text>
         </ScrollView>
 
@@ -40,7 +75,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   titulo: {
-    paddingBottom: 20,
-    fontSize: 20,
+    paddingBottom: 13,
+    fontSize: 19,
   },
+  subtitulo: {
+    color: "#de6118",
+    paddingBottom: 5,
+    fontSize: 19,
+  },
+  texto: {
+    paddingBottom: 20,
+    fontSize: 16,
+  }
 })
